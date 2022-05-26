54fe35dbab9ea513286b043b3b6342fc64e36f02
lizhiyin516 <95407084+lizhiyin516@users.noreply.github.com>
2022-05-26 19:24:47 +0200

    Add files via upload

---
+++ englishdemo2/proverb.js
@@ -0,0 +1,4 @@
+var proverb = ["Two heads are better than one.",
+"One today is worth two tomorrows.",
+"There is no royal road to science ,and only those who do not dread the fatiguing climb of gaining its numinous summits.",
+"Life is a show that never ends. Each of us is an actor. However, some people obey themselves, others please the audience."];
